e56c3b3630e8b786ab79b0e5a7d70356d5fc7e51
Melia Burnley <melia@pintsize.it>
2025-08-05 12:40:22 +0100

    CDPS-1561: 3.0 Beta 3 - Export missing middleware types (#63)

--- src/types/public/middleware/index.ts
+++ src/types/public/middleware/index.ts
@@ -1,1 +1,3 @@
 export type { default as getFrontendComponents } from '../../../middleware/getFrontendComponents'
+export type { default as retrieveCaseLoadData } from '../../../middleware/retrieveCaseLoadData'
+export type { default as retrieveAllocationJobResponsibilities } from '../../../middleware/retrieveAllocationJobResponsibilities'
