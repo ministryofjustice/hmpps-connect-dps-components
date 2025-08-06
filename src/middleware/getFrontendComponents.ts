3b919501f9dfda141ba5d45bbff65bf525214004
Melia Burnley <melia@pintsize.it>
2025-08-06 11:03:04 +0100

    CDPS-1561: 3.0 Beta 4 - Simplify middleware signatures (#65)

--- src/middleware/getFrontendComponents.ts
+++ src/middleware/getFrontendComponents.ts
@@ -1,14 +1,17 @@
 import { type RequestHandler } from 'express'
-import ComponentsService, { FrontentComponentRequestOptions } from '../componentsService'
+import ComponentsService, { FrontendComponentRequestOptions } from '../componentsService'
+
+type MiddlewareOptions = {
+  requestOptions?: FrontendComponentRequestOptions
+} & Parameters<typeof ComponentsService.create>[0]
 
 export default function getFrontendComponents({
   logger = console,
   componentApiConfig,
   authenticationClient,
   dpsUrl,
-}: Parameters<typeof ComponentsService.create>[0]): (
-  requestOptions: FrontentComponentRequestOptions,
-) => RequestHandler {
+  requestOptions,
+}: MiddlewareOptions): RequestHandler {
   const service = ComponentsService.create({ logger, componentApiConfig, authenticationClient, dpsUrl })
-  return requestOptions => service.getFrontendComponents(requestOptions)
+  return service.getFrontendComponents(requestOptions)
 }
