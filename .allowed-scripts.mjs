f4102e97038d36bd8f07303a1f95afabfebde7a2
Jon Brighton <brightonsbox@hotmail.com>
2025-12-09 13:12:14 +0000

    CDPS-1732: Using hmpps-npm-script-allowlist package

---
+++ .allowed-scripts.mjs
@@ -0,0 +1,8 @@
+import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+
+export default configureAllowedScripts({
+  allowlist: {
+    // Native solution to quickly resolve module paths, used by jest and eslint
+    'node_modules/unrs-resolver@1.11.1': 'ALLOW',
+  },
+})
