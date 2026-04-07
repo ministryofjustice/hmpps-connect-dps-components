2ea74022ebe93db2aab3841df00875e69b2215d5
Igor <igor.ushkarev1@justice.gov.uk>
2026-04-07 12:37:21 +0100

    Fix CSP updater – directives must _start_ with given names like “script-src” not simply contain them

--- src/utils/updateCsp.test.ts
+++ src/utils/updateCsp.test.ts
@@ -20,6 +20,20 @@ describe('updateCsp', () => {
     )
   })
 
+  it('should add required directives when CSP header is not set', () => {
+    const res = {
+      get: jest.fn(),
+      set: jest.fn(),
+    } as unknown as Response
+
+    updateCsp('http://fe-components', res)
+
+    expect(res.set).toHaveBeenCalledWith(
+      'content-security-policy',
+      "script-src 'self' http://fe-components;style-src 'self' http://fe-components;img-src 'self' http://fe-components;font-src 'self' http://fe-components",
+    )
+  })
+
   it('should add required directives that are not present', () => {
     const res = {
       get: jest.fn(() => "default-src 'self'"),
@@ -48,6 +62,20 @@ describe('updateCsp', () => {
     expect(res.set).toHaveBeenCalledWith(
       'content-security-policy',
       "default-src 'self';script-src 'self' http://fe-components;style-src 'self' http://fe-components;img-src 'self' http://fe-components;font-src 'self' http://fe-components",
+    )
+  })
+
+  it('should not confuse values that look like directive names', () => {
+    const res = {
+      get: jest.fn(() => "default-src 'self' http://script-src http://localhost"),
+      set: jest.fn(),
+    } as unknown as Response
+
+    updateCsp('http://fe-components', res)
+
+    expect(res.set).toHaveBeenCalledWith(
+      'content-security-policy',
+      "default-src 'self' http://script-src http://localhost;script-src 'self' http://fe-components;style-src 'self' http://fe-components;img-src 'self' http://fe-components;font-src 'self' http://fe-components",
     )
   })
 })
--- src/utils/updateCsp.ts
+++ src/utils/updateCsp.ts
@@ -7,7 +7,7 @@ export default function updateCsp(feComponentsUrl: string, res: Response) {
 
   const updatedCspDirectives = allDirectives.map(directive => {
     // if directive is not in cspToUpdate or includes fe components url already return as is
-    if (directive.includes(feComponentsUrl) || !directivesToUpdate.some(p => directive.includes(`${p} `)))
+    if (directive.includes(feComponentsUrl) || !directivesToUpdate.some(p => directive.startsWith(`${p} `)))
       return directive
 
     // if directive is in cspToUpdate and does not have fe components url, add in
@@ -15,7 +15,7 @@ export default function updateCsp(feComponentsUrl: string, res: Response) {
   })
 
   const requiredAndNotPresent = directivesToUpdate
-    .filter(p => !updatedCspDirectives.find(directive => directive.includes(`${p} `)))
+    .filter(p => !updatedCspDirectives.find(directive => directive.startsWith(`${p} `)))
     .map(p => `${p} 'self' ${feComponentsUrl}`)
 
   res.set('content-security-policy', [...updatedCspDirectives, ...requiredAndNotPresent].join(';'))
