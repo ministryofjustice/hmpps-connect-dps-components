c6b7dbcbf23facfdfeafcb52f5da2adb06970b7b
Igor <igor.ushkarev1@justice.gov.uk>
2026-04-10 16:42:21 +0100

    Refactor CSP-updating utility to work with `CspDirectives` type as now returned by MFE /components

--- src/utils/updateCsp.ts
+++ src/utils/updateCsp.ts
@@ -1,22 +1,54 @@
-import { type Response } from 'express'
+import type { Response } from 'express'
+import type { CspDirectives } from '../types/CspDirectives'
 
+// TODO: add `default-src 'self'` if header was empty?
+
+/** Update Content-Security-Policy header in response to allow use of MFE components on another domain/origin */
 export default function updateCsp(feComponentsUrl: string, res: Response) {
-  const csp = res.get('content-security-policy')
-  const allDirectives = csp?.split(';') ?? []
-  const directivesToUpdate = ['script-src', 'style-src', 'img-src', 'font-src']
+  const cspHeader = res.get('content-security-policy')
+  const directives: CspDirectives = directivesFromHeader(cspHeader)
+  const requiredDirectives = fallbackDirectives(feComponentsUrl)
+  mergeDirectives(directives, requiredDirectives)
+  const newCspHeader = headerFromDirectives(directives)
+  res.set('content-security-policy', newCspHeader)
+}
 
-  const updatedCspDirectives = allDirectives.map(directive => {
-    // if directive is not in cspToUpdate or includes fe components url already return as is
-    if (directive.includes(feComponentsUrl) || !directivesToUpdate.some(p => directive.startsWith(`${p} `)))
-      return directive
+/** Minimal known requirements to use MFE components on another domain/origin */
+function fallbackDirectives(feComponentsUrl: string): CspDirectives {
+  return {
+    'script-src': [feComponentsUrl],
+    'style-src': [feComponentsUrl],
+    'img-src': [feComponentsUrl],
+    'font-src': [feComponentsUrl],
+  }
+}
 
-    // if directive is in cspToUpdate and does not have fe components url, add in
-    return `${directive} ${feComponentsUrl}`
+function directivesFromHeader(cspHeader: string | undefined): CspDirectives {
+  return Object.fromEntries(
+    (cspHeader?.split(';') ?? []).map(line => {
+      const [directive, ...values] = line.split(/\s+/)
+      return [directive, values]
+    }),
+  )
+}
+
+function headerFromDirectives(directives: CspDirectives): string {
+  return Object.entries(directives)
+    .map(([directive, values]) => (values.length > 0 ? `${directive} ${values.join(' ')}` : directive))
+    .join(';')
+}
+
+function mergeDirectives(directives: CspDirectives, overrides: Readonly<CspDirectives>): void {
+  Object.entries(overrides).forEach(([directive, values]) => {
+    if (directive in directives) {
+      values.forEach(value => {
+        if (!directives[directive].includes(value)) {
+          directives[directive].push(value)
+        }
+      })
+    } else {
+      // eslint-disable-next-line no-param-reassign
+      directives[directive] = ["'self'", ...values]
+    }
   })
-
-  const requiredAndNotPresent = directivesToUpdate
-    .filter(p => !updatedCspDirectives.find(directive => directive.startsWith(`${p} `)))
-    .map(p => `${p} 'self' ${feComponentsUrl}`)
-
-  res.set('content-security-policy', [...updatedCspDirectives, ...requiredAndNotPresent].join(';'))
 }
