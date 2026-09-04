3d3e520aed29c44802b97c6177296c9fccce1922
Igor <igor.ushkarev1@justice.gov.uk>
2026-09-04 10:24:44 +0100

    Allow package installation

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,7 +2,9 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
   allowlist: {
+    // Needed by rollup for watching files
+    'node_modules/@parcel/watcher@2.6.0': 'ALLOW',
     // Native solution to quickly resolve module paths, used by jest and eslint
-    'node_modules/unrs-resolver@1.11.1': 'ALLOW',
+    'node_modules/unrs-resolver@1.12.2': 'ALLOW',
   },
 })
